58e5967af64bc7f8c98c1794061dabf3300c638c
skperera <85455009+skperera@users.noreply.github.com>
2021-06-06 14:03:34 -0600

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,12 @@
+# Singular-value decomposition
+from numpy import array
+from scipy.linalg import svd
+# define a matrix
+A = array([[1, 2], [3, 4], [5, 6]])
+print(A)
+# SVD
+U, s, VT = svd(A)
+print(U)
+print(s)
+print(VT)Outdoor Garden Irrigation Controller
+cat  
